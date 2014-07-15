093241a91fc03cde1fa549eabd4f65b59a088d69
Katherine Currier <kcurrier@brandeis.edu>
2014-07-15 10:59:53 -0400

    heroku stuff

--- app.js
+++ app.js
@@ -102,7 +102,10 @@ app.delete('/model/:collection/:id', function(req, res) {
 
 
 // listen on port 3000
-var port = 3000;
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+/*var port = 3000;
 app.listen(port, function() {
     console.log("server is listening on port " + port);
-}); 
+}); */
